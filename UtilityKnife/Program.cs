b5b8ac43d7121cfa8f7cb5d9dad843b701e8027f
Karionis <niklas.kanthak@online.de>
2022-01-12 21:35:01 +0100

    Added program body

--- UtilityKnife/Program.cs
+++ UtilityKnife/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace UtilityKnife
+﻿using System;
+using UtilityKnife.Converters;
+
+namespace UtilityKnife
 {
     public static class Program
     {
         static void Main(string[] args)
         {
-            // skeleton program
+            var converter = new FahrenheitToCelsius();
+            var celsius = converter.FahrenheitToCelsiusConverter(212.0);
+            Console.WriteLine($"Fahrenheit temp of 212 is {celsius}.");
         }
     }
 }
