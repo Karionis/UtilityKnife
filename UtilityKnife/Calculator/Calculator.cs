00b16b28d1967eb137cb7b2823e437e8a8a0094d
Karionis <niklas.kanthak@online.de>
2022-01-12 19:29:43 +0100

    Implement F to C conversion

--- UtilityKnife/Calculator/Calculator.cs
+++ UtilityKnife/Calculator/Calculator.cs
@@ -1,5 +1,9 @@
 ﻿namespace UtilityKnife.Calculator
 {
+    /* He modifies the Calculator class to use doubles, and
+while he is at it, he makes all the methods (and the class) static. (If you're not familiar
+with C#, don't worry about what that means; the important thing is that he's made a
+change) */
     public class Calculator
     {
         public static int Add(int x, int y)
